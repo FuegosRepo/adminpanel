87733bb95ffa21dc2fbd4313b29b60e1abf5b8dc
JugaSeguro <jugaseguroapp@gmail.com>
2025-11-18 12:50:46 +0100

    Fix: Remover base directory de netlify.toml

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-      },
-    ],
+    domains: ['localhost'],
     unoptimized: true,
   },
 }
